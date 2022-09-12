510b75a9425364482f6841d15ca1a33f62f7d387
Grant Robert Thompson <58478508+Tower5954@users.noreply.github.com>
2022-09-12 20:10:22 +0100

    Add files via upload

---
+++ booleans.py
@@ -0,0 +1,17 @@
+# ---- BOOLEANS ---- #
+
+# is equal
+print(5 == 5)
+# is greater/less
+print(5 > 5)
+# is different
+print(10 != 10)
+
+# comparisons: ==, !=, >, <, >=, <=
+
+friends = ["Rolf", "Bob"]
+abroad = ["Rolf", "Bob"]
+
+print(friends == abroad)
+
+print(friends is abroad)
